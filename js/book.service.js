47a5b3b57b60adc98ff23ad3b96a15d000513f36
Hadar-Sabag <hadarsabag5@gmail.com>
2025-03-20 15:10:54 +0200

    First commit

---
+++ js/book.service.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const gBooks = [
+    {
+        id: bg4J78,
+        title: 'Harry Potter',
+        price: 120,
+        imgUrl: 'lori-ipsi.jpg'
+    },
+    {
+        id: bg4J79,
+        title: 'Twilight',
+        price: 300,
+        imgUrl: 'lori-ipsi.jpg'
+    },
+    {
+        id: bg4J77,
+        title: 'Hunger Games',
+        price: 87,
+        imgUrl: 'lori-ipsi.jpg'
+    }
+]
+
+function getBooks() {
+    return gBooks()
+}
